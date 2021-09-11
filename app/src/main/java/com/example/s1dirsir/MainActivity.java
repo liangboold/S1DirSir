89d9ca7e01f32d98a7653dda54945cb3e0fb24ce
小曹 <1572651596@qq.com>
2021-09-11 09:18:59 +0800

    存储框架封装

--- app/src/main/java/com/example/s1dirsir/MainActivity.java
+++ app/src/main/java/com/example/s1dirsir/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.s1dirsir;
 
 
+import android.content.Context;
 import android.view.View;
 
+import com.bw.database.user.GreenDaoManager;
+import com.bw.database.user.User;
+import com.bw.database.user.UserDao;
 import com.bw.mvp_library.v.BaseActivty;
 public class MainActivity extends BaseActivty {
 
