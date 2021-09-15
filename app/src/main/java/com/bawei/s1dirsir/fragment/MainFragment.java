4f54096e117c799afef7b95fda6fcedd57496acc
小曹 <1572651596@qq.com>
2021-09-15 08:33:41 +0800

    搜索框布局

--- app/src/main/java/com/bawei/s1dirsir/fragment/MainFragment.java
+++ app/src/main/java/com/bawei/s1dirsir/fragment/MainFragment.java
@@ -7,14 +7,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.bawei.s1dirsir.R;
 
 public class MainFragment extends Fragment {
+    private EditText search;
+    private TextView scan;
+
+   private View inflate;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        inflate = inflater.inflate(R.layout.fragment_main, container, false);
+        initView();
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return inflate;
+    }
+
+    private void initView() {
+        search = (EditText) inflate.findViewById(R.id.search);
+        scan = (TextView) inflate.findViewById(R.id.scan);
     }
 }
