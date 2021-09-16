27c0702af53576edf51e89a7479b5a975a3225f0
liangboold <2868574538@qq.com>
2021-09-16 18:33:42 +0800

    手动组件化

---
+++ SortModel/src/main/java/com/bw/sortmodel/SortActivity.java
@@ -0,0 +1,14 @@
+package com.bw.sortmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SortActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sort);
+    }
+}
