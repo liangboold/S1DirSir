d8a1e427144e3ec81923b44ae0ed955dad66e621
小曹 <1572651596@qq.com>
2021-09-17 20:24:20 +0800

    主页面布局更新2

---
+++ app/src/main/java/com/bawei/s1dirsir/ImageUrl.java
@@ -0,0 +1,39 @@
+package com.bawei.s1dirsir;
+
+import com.bawei.s1dirsir.bean.DiscountBean;
+
+import java.util.ArrayList;
+
+/**
+ * @package:com.bawei.s1dirsir
+ * @fileName:ImageUrl
+ * @date on:2021/9/17 18:58
+ * @another:HG
+ * @email:1572651596@qq.com
+ */
+public class  ImageUrl {
+    public static ArrayList<DiscountBean>arrayList = new ArrayList<>();
+    static {
+        arrayList.add(new DiscountBean("https://img14.360buyimg.com/n0/jfs/t3157/231/5756125504/98807/97ab361d/588084a1N06efb01d.jpg"));
+        arrayList.add(new DiscountBean("https://img10.360buyimg.com/n7/jfs/t5905/106/1120548052/61075/6eafa3a5/592f8f7bN763e3d30.jpg"));
+        arrayList.add(new DiscountBean("https://img10.360buyimg.com/n7/jfs/t5584/99/6135095454/371625/423b9ba5/59681d91N915995a7.jpg"));
+        arrayList.add(new DiscountBean("https://img11.360buyimg.com/n7/jfs/t4447/301/1238553109/193354/13c7e995/58db19a7N25101fe4.jpg"));
+        arrayList.add(new DiscountBean("https://img14.360buyimg.com/n1/s190x190_jfs/t7525/189/155179632/395056/e200017f/598fb8a4N7800dee6.jpg"));
+        arrayList.add(new DiscountBean("https://img10.360buyimg.com/n7/jfs/t5584/99/6135095454/371625/423b9ba5/59681d91N915995a7.jpg"));
+        arrayList.add(new DiscountBean("https://img14.360buyimg.com/n0/jfs/t3157/231/5756125504/98807/97ab361d/588084a1N06efb01d.jpg"));
+    }
+
+//      imageDisounts.add(new ImageDisount("https://img14.360buyimg.com/n0/jfs/t3157/231/5756125504/98807/97ab361d/588084a1N06efb01d.jpg"));
+//        imageDisounts.add(new ImageDisount("https://img10.360buyimg.com/n7/jfs/t5905/106/1120548052/61075/6eafa3a5/592f8f7bN763e3d30.jpg"));
+//        imageDisounts.add(new ImageDisount("https://img10.360buyimg.com/n7/jfs/t5584/99/6135095454/371625/423b9ba5/59681d91N915995a7.jpg"));
+//        imageDisounts.add(new ImageDisount("https://img11.360buyimg.com/n7/jfs/t4447/301/1238553109/193354/13c7e995/58db19a7N25101fe4.jpg"));
+//        imageDisounts.add(new ImageDisount("https://img14.360buyimg.com/n1/s190x190_jfs/t7525/189/155179632/395056/e200017f/598fb8a4N7800dee6.jpg"));
+//        imageDisounts.add(new ImageDisount("https://img10.360buyimg.com/n7/jfs/t5584/99/6135095454/371625/423b9ba5/59681d91N915995a7.jpg"));
+//        imageDisounts.add(new ImageDisount("https://img14.360buyimg.com/n0/jfs/t3157/231/5756125504/98807/97ab361d/588084a1N06efb01d.jpg"));
+
+//    public final static String HOME_TOPIC_ONE ="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2069038579,1558413647&fm=26&gp=0.jpg";
+//    public final static String HOME_TOPIC_TWO ="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1975162372,3010774958&fm=26&gp=0.jpg";
+//    public final static String HOME_TOPIC_THREE ="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1034894772,889898861&fm=26&gp=0.jpg";
+//    public final static String HOME_TOPIC_FOUR ="http://img.zcool.cn/community/01796c58772f66a801219c77e4fc04.png@1280w_1l_2o_100sh.png";
+//    public final static String HOME_TOPIC_FIVE ="http://img.zcool.cn/community/0154805791ffeb0000012e7edba495.jpg@900w_1l_2o_100sh.jpg";
+}
